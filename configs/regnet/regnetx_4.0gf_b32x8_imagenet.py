ac563cdb1feb7d7d83a922f4b0bb944e7841aba0
wuwenli1 <wuwenli1@sensetime.com>
2022-04-01 14:52:37 +0800

    changepath

--- configs/regnet/regnetx_4.0gf_b32x8_imagenet.py
+++ configs/regnet/regnetx_4.0gf_b32x8_imagenet.py
@@ -8,7 +8,7 @@ _base_ = [
 file_client_args = dict(
     backend='petrel',
     path_mapping=dict({
-        '/mnt/lustre/share_data/parrots_algolib/datasets/Imagenet/': 'openmmlab:s3://openmmlab/datasets/classification/imagenet/',
+        '/mnt/lustre/share_data/PAT/datasets/Imagenet/': 'openmmlab:s3://openmmlab/datasets/classification/imagenet/',
     }))
 # file_client_args = dict(backend='disk')
 dataset_type = 'ImageNet'
