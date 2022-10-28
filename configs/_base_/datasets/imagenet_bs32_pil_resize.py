eff8d77094b17d4069c50ad50d581e32a06f2b49
sunxiaoye <sunxiaaoye@sensetime.com>
2022-10-28 12:14:58 +0800

    for v0.22test

--- configs/_base_/datasets/imagenet_bs32_pil_resize.py
+++ configs/_base_/datasets/imagenet_bs32_pil_resize.py
@@ -1,13 +1,11 @@
 # dataset settings
-file_client_args = dict(
-    backend='petrel',
-    path_mapping=dict({
-        '/mnt/lustre/share_data/PAT/datasets/Imagenet/': 'openmmlab:s3://openmmlab/datasets/classification/imagenet/',
-    }))
-# file_client_args = dict(backend='disk')
 dataset_type = 'ImageNet'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# use file client petrel
+file_client_args = dict(backend='petrel')
+data_prefix = 'openmmlab:s3://openmmlab/datasets/classification/imagenet/'
+
 train_pipeline = [
     dict(type='LoadImageFromFile', file_client_args=file_client_args),
     dict(type='RandomResizedCrop', size=224, backend='pillow'),
@@ -30,17 +28,18 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        data_prefix='/mnt/lustre/share_data/PAT/datasets/Imagenet/train',
+        data_prefix=data_prefix + 'train/',
+        ann_file='/mnt/lustre/share/wangchenyu/imagenet/meta/train.txt',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        data_prefix='/mnt/lustre/share_data/PAT/datasets/Imagenet/val',
-        ann_file='/mnt/lustre/share_data/PAT/datasets/Imagenet/meta/val.txt',
+        data_prefix=data_prefix + 'val/',
+        ann_file='/mnt/lustre/share/wangchenyu/imagenet/meta/val.txt',
         pipeline=test_pipeline),
     test=dict(
         # replace `data/val` with `data/test` for standard test
         type=dataset_type,
-        data_prefix='/mnt/lustre/share_data/PAT/datasets/Imagenet/val',
-        ann_file='/mnt/lustre/share_data/PAT/datasets/Imagenet/meta/val.txt',
+        data_prefix=data_prefix + 'val/',
+        ann_file='/mnt/lustre/share/wangchenyu/imagenet/meta/val.txt',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='accuracy')
