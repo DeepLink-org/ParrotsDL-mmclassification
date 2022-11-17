ef965ff05e63f21f8ca6f896aaf6133f2e689f3a
youhuijie <st11279@qq.com>
2022-11-17 18:43:28 +0800

    for path

--- configs/_base_/datasets/imagenet_bs32.py
+++ configs/_base_/datasets/imagenet_bs32.py
@@ -24,17 +24,17 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        data_prefix='data/imagenet/train',
+        data_prefix='/mnt/lustre/share/images/train',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        data_prefix='data/imagenet/val',
-        ann_file='data/imagenet/meta/val.txt',
+        data_prefix='/mnt/lustre/share/images/val',
+        ann_file='/mnt/lustre/share/images/meta/val.txt',
         pipeline=test_pipeline),
     test=dict(
         # replace `data/val` with `data/test` for standard test
         type=dataset_type,
-        data_prefix='data/imagenet/val',
-        ann_file='data/imagenet/meta/val.txt',
+        data_prefix='/mnt/lustre/share/images/val',
+        ann_file='/mnt/lustre/share/images/meta/val.txt',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='accuracy')
